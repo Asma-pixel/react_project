9cee7e996cba99423d06c9c9db861548d7e83411
Vladimir Plotvinov <vladimir.plotvinov@gmail.com>
2020-11-02 18:51:11 +0300

    Link bonus.js

--- src/Components/content/feed/bonus.js
+++ src/Components/content/feed/bonus.js
@@ -1,106 +1,114 @@
-import React, { useState } from "react";
+import React, {useContext, useState} from "react";
 import Option from "./feed_img/opt.svg";
 import Context from "../Context";
 import "./feed.css";
-import { useContext } from "react";
 import Comments from "./comments/comments";
 import AddComments from "./comments/addComments";
-function Bonus({ bon }) {
-  const { deleteBonus } = useContext(Context);
-  const [del, setDel] = useState(null);
-  const [comm, setComm] = useState(null);
-  document.ondblclick = () => {
-    setDel(false);
-  };
-  const [comments, setComments] = useState([]);
-  function addComment(comment) {
-    setComments(
-      comments.concat([
-        {
-          id: Date.now(),
-          comment,
-        },
-      ])
-    );
-  }
-   const[line,setLine]=useState(false);
-  let length =comments.length;
-  function lineBefore(length, comm){  
-    if (length !==0 || comm !== 0 ){ setLine(true)}
-    else setLine(false);
-    console.log("comm="+comm);
-    console.log("comments="+length);
-    console.log(line);
-  }
-let classes = "over";
- if(del === false){
-  classes="over_hide";
-} else if(del === true) {classes="over_show";}
 
-  return (
-    <div>
-      <div className="box-bigger">
-        <div className="second-wrap-of-box">
-          <div className="first-content">
-            <div className="profiletitle">
-              <div className="image-circle"></div>
-              <span className="title-name">Monica Bellucci</span>
-            </div>
-            <form className="parent-of-options-and-conditions">
-              <h2 className="condition">just now</h2>
-              <img
-                className="option"
-                src={Option}
-                alt="opt"
-                onClick={() => {
-                  setDel(true);
-                }}
-              />
-              <ul className={classes}>
-                <li onClick={() => deleteBonus(bon.id)}>Удалить</li>
-                <li className="important">Важное</li>
-              </ul>
-            </form>
-          </div>
-          <div className="wrap-first-text">
-            <div className="first-block-text">
+function Bonus({bon}) {
+    const {deleteBonus} = useContext(Context);
+    const [del, setDel] = useState(null);
+    const [comm, setComm] = useState(null);
+    document.ondblclick = () => {
+        setDel(false);
+    };
+    const [comments, setComments] = useState([]);
+
+    function addComment(comment) {
+        setComments(
+            comments.concat([
+                {
+                    id: Date.now(),
+                    comment,
+                },
+            ])
+        );
+    }
+
+    const [line, setLine] = useState(false);
+    let length = comments.length;
+
+    function lineBefore(length, comm) {
+        if (length !== 0 || comm !== 0) {
+            setLine(true)
+        } else setLine(false);
+        console.log("comm=" + comm);
+        console.log("comments=" + length);
+        console.log(line);
+    }
+
+    let classes = "over";
+    if (del === false) {
+        classes = "over_hide";
+    } else if (del === true) {
+        classes = "over_show";
+    }
+
+    return (
+        <div>
+            <div className="box-bigger">
+                <div className="second-wrap-of-box">
+                    <div className="first-content">
+                        <div className="profiletitle">
+                            <div className="image-circle"></div>
+                            <span className="title-name">Monica Bellucci</span>
+                        </div>
+                        <form className="parent-of-options-and-conditions">
+                            <h2 className="condition">just now</h2>
+                            <img
+                                className="option"
+                                src={Option}
+                                alt="opt"
+                                onClick={() => {
+                                    setDel(true);
+                                }}
+                            />
+                            <ul className={classes}>
+                                <li onClick={() => deleteBonus(bon.id)}>Удалить</li>
+                                <li className="important">Важное</li>
+                            </ul>
+                        </form>
+                    </div>
+                    <div className="wrap-first-text">
+                        <div className="first-block-text">
               <span className="first-block-text first-block-text-1st">
                 {`+ ${bon.point} `}
-                <span className="first-block-text first-block-text-1st-normal">
+                  <span className="first-block-text first-block-text-1st-normal">
                   to
                 </span>
                 <a
-                  className="first-block-text first-block-text first-block-text-1st first-block-text first-block-text-1st-link"
-                  href=""
+                    className="first-block-text first-block-text first-block-text-1st first-block-text first-block-text-1st-link"
+                    href=""
                 >
                   {`  ${bon.name}`}
                 </a>
               </span>
-              <p className="first-block-text first-block-text-2nd">
-                {bon.reason}
-              </p>
-              <img className="man-image" src="" />
-              <div
-                className="addCommentBlock">
-                <div className={comm ? "commentImageYellow":"commentImage"} onClick={() => {
-                  setComm(Math.abs(comm*1-1));
-                  lineBefore(length,comm);
-                
-                }}></div>
-                <div className="plusBonus"></div>
-              </div>
-              <div>
-              <div className = {line ? "commentList": "hiddenCommentList"}>
-              <Comments comments={comments} key={comments.id} line={line}  />
-              <AddComments comm={comm} onCreate={addComment}/> 
-              </div>
-              </div>
+                            <p className="first-block-text first-block-text-2nd">
+                                {bon.reason}
+                            </p>
+                            <img className="man-image" src=""/>
+                            <div
+                                className="addCommentBlock">
+                                <div className={comm ? "commentImageYellow" : "commentImage"} onClick={() => {
+                                    setComm(Math.abs(comm * 1 - 1));
+                                    lineBefore(length, comm);
+
+                                }}></div>
+                                <div className="plusBonus"></div>
+                            </div>
+                            <div>
+                                <div className={line ? "commentList" : "hiddenCommentList"}>
+                                    <Comments comments={comments} key={comments.id} line={line}/>
+                                    <AddComments comm={comm} onCreate={addComment}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-          </div>
         </div>
-      
-      </div>
-    </div>
-  );
+    );
 }
+
 export default Bonus;
